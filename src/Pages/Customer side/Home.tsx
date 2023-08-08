57ce779ce773ab4570e672c33cca09557d62130d
zaynab18 <zaynabemadi@gmail.com>
2023-08-08 18:36:41 +0330

    import header to home

--- src/Pages/Customer side/Home.tsx
+++ src/Pages/Customer side/Home.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React from "react";
+import Header from "../../Layout/Header";
+import { Outlet } from "react-router-dom";
 
 function Home() {
     return (
-        <div></div>
+        <>
+            <Header />
+            <Outlet />
+        </>
     );
 }
 
