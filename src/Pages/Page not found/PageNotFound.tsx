99a68510f34aeee5bc8e8293adfd9b558301790c
zaynab18 <zaynabemadi@gmail.com>
2023-08-11 23:06:14 +0330

    add page not found

--- src/Pages/Page not found/PageNotFound.tsx
+++ src/Pages/Page not found/PageNotFound.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import ImgElement from '../../Assets/Images/page not found.jpg';
+import Box from "@mui/material/Box";
+import {Button, Grid} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 
 function PageNotFound() {
+    const navigate = useNavigate();
     return (
-        <div>PageNotFound</div>
+        <>
+            <Grid container
+                  direction="column"
+                  justifyContent="center"
+                  alignItems="center"
+                  sx = {{backgroundColor : '#1F1D28' , height : '100vh'}}
+            >
+            <Box src={ImgElement} component={'img'} />
+                <Button variant="text" sx = {{color : '#E3FFE7'}} size="large" onClick={() => navigate('/')} >Return</Button>
+            </Grid>
+        </>
     );
 }
 
