e297b8906167be640271ffb3c460dc91adde795c
zaynab18 <zaynabemadi@gmail.com>
2023-08-12 23:53:42 +0330

    add server file

---
+++ src/Config/Constant.tsx
@@ -0,0 +1,6 @@
+export const CRUD_MODE_UPDATE : string = "CRUD_MODE_UPDATE";
+export const CRUD_MODE_CREATE : string= "CRUD_MODE_CREATE";
+
+export const ACCESS_TOKEN  : string= "ACCESS_TOKEN";
+export const REFRESH_TOKEN : string = "REFRESH_TOKEN";
+export const IS_LOGGED_IN : string = "IS_LOGGGED_IN";
--- src/Config/api.tsx
+++ src/Config/api.tsx
@@ -1,8 +1,8 @@
 export const BASE_URL : string = 'http://localhost:3002/';
-// export const PRODUCT_URL = "http://localhost:3002/products";
-// export const CATEGORY_URL = "http://localhost:3002/categories";
-// export const SUBGROUP_URL = "http://localhost:3002/subgroups";
-// export const SUBCATEGORY_URL = "http://localhost:3002/subcategories";
-// export const ORDERS_URL = "http://localhost:3002/orders";
-export const LOGIN_URL = "/auth/login";
-export const REFRESH_TOKEN_URL = "/auth/refresh-token";
+// export const PRODUCT_URL : string= "http://localhost:3002/products";
+// export const CATEGORY_URL : string= "http://localhost:3002/categories";
+// export const SUBGROUP_URL: string = "http://localhost:3002/subgroups";
+// export const SUBCATEGORY_URL: string = "http://localhost:3002/subcategories";
+// export const ORDERS_URL: string = "http://localhost:3002/orders";
+export const LOGIN_URL : string= "/auth/login";
+export const REFRESH_TOKEN_URL : string = "/auth/refresh-token";
