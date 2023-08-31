a1929fe96c3f88b11d75bc32f0e0f16d4a304b71
zaynab18 <zaynabemadi@gmail.com>
2023-08-31 20:47:36 +0330

    add authentication with error

--- src/Pages/Admin panel/AdminLogin.tsx
+++ src/Pages/Admin panel/AdminLogin.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   Button,
   Checkbox,
@@ -20,26 +20,50 @@ import {
 } from "../../Assets/Styles/AdminLoginStyle";
 import BrandLogo from "../../Assets/Images/whiteLogo.svg";
 import Box from "@mui/material/Box";
-import { useNavigate } from "react-router-dom";
+import {Navigate, useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {login} from "../../Middleware/Redux/userSlice";
+import {AppDispatch, RootState} from "../../Middleware/Redux/store";
+
+
+//type declaring
+interface FormDataType {
+  userName : string,
+  password : string,
+}
+
 
 function AdminLogin() {
   const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+  const {error , isLoggedIn} = useSelector((state : RootState) => state.user)
+  //show password button
   const [showPassword, setShowPassword] = React.useState(false);
   const handleClickShowPassword = () => setShowPassword((show) => !show);
-
+  //form data
+  const [formData , setFormData] = useState<FormDataType>({
+    userName : "",
+    password : ""
+  })
+  //onChange for userName and password
+const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+  setFormData({
+    ...formData,
+    [e.currentTarget.id] : e.currentTarget.value
+  })
+};
   const handleMouseDownPassword = (
     event: React.MouseEvent<HTMLButtonElement>
   ) => {
     event.preventDefault();
   };
-  // const handleSubmit = (event: any) => {
-  //   event.preventDefault();
-  //   const data = new FormData(event.currentTarget);
-  //   console.log({
-  //     userName: data.get('userName'),
-  //     password: data.get('password'),
-  //   });
-  // };
+  //handle authentication
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    dispatch(login(formData))
+  };
+  if (isLoggedIn) return <Navigate to={"/Admin-dashboard"}/>
+
   return (
     <>
       <Grid
@@ -56,9 +80,11 @@ function AdminLogin() {
           <Typography variant="h4" sx={{ m: 1 }}>
             Management panel
           </Typography>
+          {/*error place*/}
+          {error && <h6 className="error">{error}</h6>}
           {/*userName*/}
-          <FormControl sx={{ m: 1 }} fullWidth id="userName">
-            <TextField required label="User Name" variant="outlined" />
+          <FormControl sx={{ m: 1 }} fullWidth  >
+            <TextField required label="User Name" variant="outlined" onChange={handleChange} id="userName"/>
           </FormControl>
           {/*password*/}
           <FormControl sx={{ m: 1 }} variant="outlined" fullWidth required>
@@ -66,6 +92,7 @@ function AdminLogin() {
             <OutlinedInput
               id="password"
               type={showPassword ? "text" : "password"}
+              onChange={handleChange}
               endAdornment={
                 <InputAdornment position="end">
                   <IconButton
@@ -99,7 +126,8 @@ function AdminLogin() {
           <Button
             variant="contained"
             type="submit"
-            onClick={() => navigate("/Admin-dashboard")}
+            // onClick={() => navigate("/Admin-dashboard")}
+              onClick={handleSubmit}
           >
             Submit
           </Button>
