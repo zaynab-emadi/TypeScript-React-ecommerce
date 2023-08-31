5f4c102d3bf17a71a74a67fde8a8f67bfef5021d
zaynab18 <zaynabemadi@gmail.com>
2023-08-31 18:15:48 +0330

    add request interceptor

--- src/Api/axios.tsx
+++ src/Api/axios.tsx
@@ -1,6 +1,19 @@
 import axios from "axios";
-import {BASE_URL} from "../Config/api";
+import {BASE_URL, LOGIN_URL, REFRESH_TOKEN_URL} from "../Config/api";
+import {AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse , InternalAxiosRequestConfig} from "axios";
+import {ACCESS_TOKEN, REFRESH_TOKEN} from "../Config/Constant";
+
 
 axios.defaults.baseURL = BASE_URL;
+
+axios.interceptors.request.use((config : InternalAxiosRequestConfig) : InternalAxiosRequestConfig=> {
+    if (config.url === REFRESH_TOKEN_URL) {
+        config.headers.refreshToken = localStorage.getItem(REFRESH_TOKEN);
+    } else if (config.url !== LOGIN_URL) {
+        config.headers.token = localStorage.getItem(ACCESS_TOKEN);
+    }
+    return config
+} );
+
 
 export default axios;
