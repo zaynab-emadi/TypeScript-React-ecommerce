2ddc3dfad542c7c80f965fedabc22e38d7ea9e7c
zaynab18 <zaynabemadi@gmail.com>
2023-08-31 18:07:42 +0330

    add comment

--- src/Config/api.tsx
+++ src/Config/api.tsx
@@ -1,8 +1,9 @@
+// base url
 export const BASE_URL : string = 'http://localhost:3002/';
-// export const PRODUCT_URL : string= "http://localhost:3002/products";
-// export const CATEGORY_URL : string= "http://localhost:3002/categories";
-// export const SUBGROUP_URL: string = "http://localhost:3002/subgroups";
-// export const SUBCATEGORY_URL: string = "http://localhost:3002/subcategories";
-// export const ORDERS_URL: string = "http://localhost:3002/orders";
+
+// post
+
+
+//user
 export const LOGIN_URL : string= "/auth/login";
 export const REFRESH_TOKEN_URL : string = "/auth/refresh-token";
